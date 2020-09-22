e4dc8e632514aed72e898ed60e5d95ffa720b7ea
hassan khaled <hasan.khaled.fcis@gmail.com>
2020-09-22 14:21:53 +0200

    dynamic link generation

--- website/app.js
+++ website/app.js
@@ -7,6 +7,20 @@ themes = [{text:'orange'}, {text:'brown'}, {text:'light'},{text:'white'}, {text:
 {text:'yellow'}, {text:'paleBlue'}, {text:'paleBiege'}, {text:'green'},{text:'pale'}, {text:'blue'}];
 
 
+
+/** @constant
+    @type {array}
+    @global
+    @description Hold links' names and hrefs.
+*/
+links = [ {link:"",text:"Search"},
+          {link:"",text:"Schedule"},
+          {link:"",text:"Shows"},
+          {link:"",text:"Episodes"},
+          {link:"",text:"People"}];
+
+
+
 /** @constant
     @type {object}
     @global
@@ -15,20 +29,74 @@ themes = [{text:'orange'}, {text:'brown'}, {text:'light'},{text:'white'}, {text:
 let dropdown = document.querySelector(".dropdown-menu");
 
 
+/** @constant
+    @type {object}
+    @global
+    @description Hold reference nav bar.
+*/
+let navbar = document.querySelector(".navbar-nav");
+
 /**
 * @function  fillInThemes
 * @description fill in the themes in the dropdown list .
 */
-fillInThemes = ()=>{
+fillInThemes = ()=> {
     for( theme of themes){
         let temp = document.createElement("a");
-       // temp.setAttribute("innerHTML",theme.text);
         temp.classList.add("dropdown-item");
         temp.appendChild(document.createTextNode(theme.text));
-        
         dropdown.appendChild(temp);
     }
 }
+
+fillInThemes();
+/**
+* @function  createAnchorFromTextAndHref
+* @description fill in the linkes in the nav bar and highlighted active link .
+* @param text to be shown inside the anchor 
+* @param href to be used in teh anchor 
+* @returns object to refrences to anchor 
+*/
+createAnchorFromTextAndHref = ( text, href)=>{
+    let anchor = document.createElement("a");
+    anchor.appendChild(document.createTextNode(text));
+    anchor.setAttribute("href",href);
+    anchor.classList.add("nav-link");
+    return anchor;
+}
+
+/**
+* @function  createListItemWithAnchor
+* @description fill in the linkes in the nav bar and highlighted active link .
+* @param activeLink to be highlighted active in nav bar
+*/
+createListItemWithAnchor = (text, href) => {
+    let tempLi = document.createElement("li");
+    tempLi.classList.add("nav-item");
+    tempLi.appendChild(createAnchorFromTextAndHref(text,href));
+    console.log(tempLi);
+
+    navbar.appendChild(tempLi);
+}
+
+
+/**
+* @function  fillInLinksInNavBar
+* @description fill in the linkes in the nav bar and highlighted active link .
+* @param activeLink to be highlighted active in nav bar
+*/
+fillInLinksInNavBar = ()=> {
+    for( link of links){
+        createListItemWithAnchor(link.text,link.href);
+    }
+;
+}
+
+fillInLinksInNavBar();
+
+
+
+
 
 
 /**
