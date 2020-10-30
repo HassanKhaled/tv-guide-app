0da5258aa742700a5bd85ae53d6359eb7cb7632a
hassan khaled <hasan.khaled.fcis@gmail.com>
2020-10-30 13:42:35 +0200

    add splash screen with fade in animation

--- website/js/home.js
+++ website/js/home.js
@@ -1,1 +1,11 @@
+const splash = document.querySelector(".splash");
+
+
+document.addEventListener("DOMContentLoaded",(e)=>{
+setTimeout(()=>{
+    splash.classList.add("display-none");
+
+},2000);
+
+});
 callOnStart("Home",false);
